69cf1d5b9dec6ffdac0b2a7ff0c133c75ee57de1
Kalyuzhnyy Dmitry <difrom_pro@mail.ru>
2025-10-29 18:00:48 +0300

    change_1 def filter

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 import os.path
-
 PATH_TO_TXT = os.path.join(os.getcwd(), "data", "names.txt")
-with open(PATH_TO_TXT, "r", encoding="utf-8") as f:
-    list_txt = f.readlines()
-print(list_txt)
 
-def filter_name_from_txt(name_list: list[str]) -> list[str]:
+def filter_name_from_txt(file_name):
+    # with open('data/'+file_name, "r", encoding="utf-8") as f:
+    with open(PATH_TO_TXT, "r", encoding="utf-8") as f:
+        list_txt = f.readlines()
+    print(list_txt)
+
     new_name_list = []
     signs = ".,!? \n@#№;:(){}[]"
     # new_name = ""
-    for name in name_list:
-        new_name = "".join(
-            char for char in name if char not in signs and not char.isdigit()
-        )
+    for name in list_txt:
+        new_name = "".join(char for char in name if char.isalpha())
         new_name_list.append(new_name) if new_name != "" else None
 
     return new_name_list
 
 
-print(filter_name_from_txt(list_txt))
+if __name__ == "__main__":
+    filtered_list_names = filter_name_from_txt("names.txt")
+    for i in filtered_list_names:
+        print(i)
