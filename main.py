4ce1b543cd8446ba1848f82443f881a9d8e8b819
Kalyuzhnyy Dmitry <difrom_pro@mail.ru>
2025-10-29 23:41:21 +0300

    add filter rus and latin words

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import os.path
+import re
+
+# Получаем путь к файлу names.txt
 PATH_TO_TXT = os.path.join(os.getcwd(), "data", "names.txt")
 
+
 def filter_name_from_txt(file_name):
-    # with open('data/'+file_name, "r", encoding="utf-8") as f:
+    """Функция отсортировывает имена при этом удаляет числа и знаки припинания."""
+    # Преобразуем текст в список
+    # with open('data/'+file_name, "r", encoding="utf-8") as f: - альтернативный вариант
     with open(PATH_TO_TXT, "r", encoding="utf-8") as f:
         list_txt = f.readlines()
-    print(list_txt)
 
     new_name_list = []
     signs = ".,!? \n@#№;:(){}[]"
     # new_name = ""
+    # Проверяем слово на лишние символы и удаляем их.
     for name in list_txt:
         new_name = "".join(char for char in name if char.isalpha())
         new_name_list.append(new_name) if new_name != "" else None
@@ -17,7 +23,38 @@ def filter_name_from_txt(file_name):
     return new_name_list
 
 
+def is_cirillic(name_item: str) -> bool:
+    return bool(re.search('[а-яА-Я]', name_item))
+
+def is_latin(name_item: str) -> bool:
+    return bool(re.search('[a-zA-Z]', name_item))
+
+
+def filter_russian_names(names_list: list[str]) -> list[str]:
+    new_list = []
+    for name in names_list:
+        if is_cirillic(name):
+            new_list.append(name)
+    return new_list
+
+def filter_latin_names(names_list: list[str]) -> list[str]:
+    new_list = []
+    for name in names_list:
+        if is_latin(name):
+            new_list.append(name)
+    return new_list
+
+def save_to_file(file_name: str, data: str) -> None:
+    with open('data/'+file_name, "w", encoding="utf-8") as names_file:
+        names_file.write(data)
+
+
 if __name__ == "__main__":
     filtered_list_names = filter_name_from_txt("names.txt")
     for i in filtered_list_names:
         print(i)
+
+    save_to_file("russian_words.txt",
+                 "\n".join(filter_russian_names(filtered_list_names)))
+    save_to_file("latin_words.txt",
+                 "\n".join(filter_latin_names(filtered_list_names)))
